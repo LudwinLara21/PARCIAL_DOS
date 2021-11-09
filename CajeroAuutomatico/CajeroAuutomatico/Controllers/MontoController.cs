7e795538267de5cc463e23db4a6a348c46eb6c36
LudwinLara21 <87671764+LudwinLara21@users.noreply.github.com>
2021-11-09 10:42:14 -0600

    Ultima actualizacion

---
+++ CajeroAuutomatico/CajeroAuutomatico/Controllers/MontoController.cs
@@ -0,0 +1,53 @@
+﻿using CajeroAuutomatico.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CajeroAuutomatico.Controllers
+{
+    public class MontoController : Controller
+    {
+        // GET: Monto
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+    
+    public ActionResult index (ModeloGeneral K)
+    {
+        _ = K;
+
+        if (K.Monto % 5==0)
+     
+        {
+            return View("SI");
+           
+        }
+        return View("NO");
+        
+
+      
+    }
+    public ActionResult SI()
+        {
+
+        {
+            return View();
+        }
+
+
+
+    }
+    public ActionResult NO()
+
+    {
+        {
+            return View();
+        }
+    }
+}
+
+
---
+++ CajeroAuutomatico/CajeroAuutomatico/Models/ModeloGeneral.cs
@@ -0,0 +1,9 @@
+﻿namespace CajeroAuutomatico.Models
+{
+    public class ModeloGeneral
+    {
+        private int monto;
+
+        public int Monto { get => monto; set => monto = value; }
+    }
+}
